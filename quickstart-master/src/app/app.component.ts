2d2dba7567ab0966b4fbce2fb1a82abd9126079e
Eren Brumley <eren.brumley@daugherty.com>
2017-05-15 11:24:06 -0400

    adding http call

--- quickstart-master/src/app/app.component.ts
+++ quickstart-master/src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { PeopleService } from './people.service';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1> {{title}} </h1>
+    <router-outlet>
+  `,
+  // This registers the PeopleService , now Angular 2 knows to inject it when required
+  providers: [PeopleService]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title:string = 'Star Wars People!';
+}
---
+++ quickstart-master/src/app/person-details.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Person } from './person';
+import { PeopleService } from './people.service';
+
+@Component({
+  selector: 'person-details',
+  templateUrl: 'app/person-details.component.html'
+})
+export class PersonDetailsComponent implements OnInit, OnDestroy {
+  person: Person;
+  sub: any;
+  professions: string[] = ['jedi', 'bounty hunter', 'princess', 'sith lord'];
+
+  constructor(private peopleService: PeopleService,
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      // here is how to get the parameter id number from the url
+      let id = Number.parseInt(params['id']);
+      console.log('getting person with id: ', id);
+      this.peopleService
+        .get(id)
+        .subscribe(p => this.person = p);
+    });
+  }
+
+  ngOnDestroy() {
+    // to avoid memory leaks
+    this.sub.unsubscribe();
+  }
+
+  gotoPeoplesList() {
+    let link = ['/persons'];
+    this.router.navigate(link);
+    // you can also use the window.history API instead of the two previous lines:
+    // window.history.back();
+  }
+
+}
